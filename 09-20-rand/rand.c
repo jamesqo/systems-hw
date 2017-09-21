a14ba9899522d55fb107d3d67d066e850ff723b6
James Ko <jamesqko@gmail.com>
2017-09-21 16:04:51 -0400

    Added hw for 09-21

---
+++ 09-20-rand/rand.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int first[10];
+    int second[10];
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < 10; i++) {
+        first[i] = rand();
+    }
+
+    first[9] = 0;
+
+    printf("%s\n", "The original array:");
+
+    for (i = 0; i < 10; i++) {
+        printf("first[%d]: %d\n", i, first[i]);
+    }
+
+    int* second_ptr = second;
+
+    printf("%s\n", "The second array:");
+
+    for (i = 0; i < 10; i++) {
+        second[i] = first[9 - i];
+	printf("second[%d]: %d\n", i, second[i]);
+    }
+
+    return 0;
+}
